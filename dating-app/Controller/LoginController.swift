f91c0973c7aa5233638272b84ca8e1484a52332b
Yogin Kumar Suttroogun <ysuttroogun@astek.mu>
2018-12-04 15:22:22 +0400

    Design login screen - pt 2

--- dating-app/Controller/LoginController.swift
+++ dating-app/Controller/LoginController.swift
@@ -9,15 +9,19 @@
 import UIKit
 
 class LoginController: UIViewController {
-
+    
     @IBOutlet weak var tableView: UITableView! {
         didSet {
             tableView.dataSource = self
             tableView.delegate = self
         }
     }
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var pageControl: UIPageControl!
     
     var loginOptions = [String]()
+    var loginImg: [String] = ["0","1","2"]
+    var frame = CGRect(x: 0, y: 0, width: 0, height: 0)
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,6 +33,19 @@ class LoginController: UIViewController {
         loginOptions = ["Login with Facebook","Login with Phone number"]
         let nibName = UINib(nibName: "LoginTableViewCell", bundle: nil)
         tableView.register(nibName, forCellReuseIdentifier: "loginCell")
+        
+        pageControl.numberOfPages = loginImg.count
+        for index in 0..<loginImg.count {
+            frame.origin.x = scrollView.frame.size.width * CGFloat(index)
+            frame.size = scrollView.frame.size
+            
+            let imgView = UIImageView(frame: frame)
+            imgView.image = UIImage(named: loginImg[index])
+            self.scrollView.addSubview(imgView)
+        }
+        
+        scrollView.contentSize = CGSize(width: (scrollView.frame.size.width * CGFloat(loginImg.count)), height: scrollView.frame.size.height)
+        scrollView.delegate = self
     }
 }
 
@@ -45,6 +62,12 @@ extension LoginController: UITableViewDataSource, UITableViewDelegate {
         }
         return UITableViewCell()
     }
-    
-    
+}
+
+// MARK:- UIScrollViewDelegate
+extension LoginController: UIScrollViewDelegate {
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        let pageNumber = scrollView.contentOffset.x / scrollView.frame.size.width
+        pageControl.currentPage = Int(pageNumber)
+    }
 }
