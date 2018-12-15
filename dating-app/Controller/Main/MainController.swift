4923722bcdd2807978f1210d7a9ee3e7bcd24b45
SUTTROOGUN Yogin Kumar <ysuttroogun>
2018-12-15 18:18:48 +0400

    Adding top and bottom design for the dashboard

---
+++ dating-app/Controller/Main/MainController.swift
@@ -0,0 +1,33 @@
+//
+//  MainController.swift
+//  dating-app
+//
+//  Created by SUTTROOGUN Yogin Kumar on 15/12/2018.
+//  Copyright © 2018 SUTTROOGUN Yogin Kumar. All rights reserved.
+//
+
+import UIKit
+
+class MainController: UIViewController {
+
+    let topStackView = TopNavigationStackView()
+    let blueView = UIView()
+    let buttonsStackView = HomeBottomControlsStackView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        blueView.backgroundColor = .blue
+        setupLayout()
+    }
+    
+    // MARK:- Fileprivate
+    
+    fileprivate func setupLayout() {
+        let overallStackView = UIStackView(arrangedSubviews: [topStackView, blueView, buttonsStackView])
+        overallStackView.axis = .vertical
+        view.addSubview(overallStackView)
+        overallStackView.anchor(top: view.safeAreaLayoutGuide.topAnchor, leading: view.leadingAnchor, bottom: view.safeAreaLayoutGuide.bottomAnchor, trailing: view.trailingAnchor)
+    }
+
+}
