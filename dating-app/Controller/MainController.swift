d7988f7def7590da78c609a1e60e4f232b57fed5
SUTTROOGUN Yogin Kumar <ysuttroogun>
2018-12-15 17:55:36 +0400

    Adding main view controller for app

---
+++ dating-app/Controller/MainController.swift
@@ -0,0 +1,17 @@
+//
+//  MainController.swift
+//  dating-app
+//
+//  Created by SUTTROOGUN Yogin Kumar on 15/12/2018.
+//  Copyright © 2018 SUTTROOGUN Yogin Kumar. All rights reserved.
+//
+
+import UIKit
+
+class MainController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
