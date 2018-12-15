697442e4878144823653cafcf4682a61dfe9684c
SUTTROOGUN Yogin Kumar <ysuttroogun>
2018-12-15 21:59:40 +0400

    Handling rotation and card dismiss

--- dating-app/Controller/Main/CardView.swift
+++ dating-app/Controller/Main/CardView.swift
@@ -12,6 +12,9 @@ class CardView: UIView {
     
     fileprivate let imgView = UIImageView(image: #imageLiteral(resourceName: "lady5c"))
     
+//    Configuration
+    fileprivate let threshold: CGFloat = 80
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         
@@ -31,7 +34,7 @@ class CardView: UIView {
         case .changed:
             handleChanged(gesture)
         case .ended:
-            handleEnded()
+            handleEnded(gesture)
         default:
             ()
         }
@@ -39,14 +42,28 @@ class CardView: UIView {
     
     fileprivate func handleChanged(_ gesture: UIPanGestureRecognizer) {
         let translation = gesture.translation(in: nil)
-        self.transform = CGAffineTransform(translationX: translation.x, y: translation.y)
+        
+//        Rotation
+        let degrees: CGFloat = translation.x / 20
+        let angle = degrees * .pi / 180
+        
+        let rotationalTransformation = CGAffineTransform(rotationAngle: angle)
+        self.transform = rotationalTransformation.translatedBy(x: translation.x, y: translation.y)
     }
     
-    fileprivate func handleEnded() {
+    fileprivate func handleEnded(_ gesture: UIPanGestureRecognizer) {
+        let translationDirection: CGFloat = gesture.translation(in: nil).x > 0 ? 1 : -1
+        let shouldDismissCard = abs(gesture.translation(in: nil).x) > threshold
+        
         UIView.animate(withDuration: 0.75, delay: 0, usingSpringWithDamping: 0.6, initialSpringVelocity: 0.1, options: .curveEaseOut, animations: {
-            self.transform = .identity
+            if shouldDismissCard {
+                self.frame = CGRect(x: 1000 * translationDirection, y: 0, width: self.frame.width, height: self.frame.height)
+            }else {
+                self.transform = .identity
+            }
         }) { (_) in
-            
+            self.transform = .identity
+            self.frame = CGRect(x: 0, y: 0, width: self.superview!.frame.width, height: self.superview!.frame.height)
         }
     }
     
